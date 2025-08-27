5947baffed5b49d733481468400f4cdf723ce2d8
Jaraad Kamal <jkamal@pistevodecision.com>
2025-08-27 14:14:27 -0400

    Fixing syntax issues

--- src/edi835Parsing/library.ts
+++ src/edi835Parsing/library.ts
@@ -21,7 +21,7 @@ export function create835Tables(): SqliteDatabaseType {
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.HEADER_TABLE} (
       id            INTEGER PRIMARY KEY AUTOINCREMENT,
-      created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`
   );
 
@@ -29,22 +29,22 @@ export function create835Tables(): SqliteDatabaseType {
     `CREATE TABLE IF NOT EXISTS ${constants.ST_TABLE} (
       id                        INTEGER PRIMARY KEY AUTOINCREMENT,
       segment_order             INTEGER NOT NULL,
-      
+
       id_code                   VARCHAR(3) NOT NULL,
       transaction_set_control   VARCHAR(9) NOT NULL,
       convention_reference      VARCHAR(35),
-      
+
       x12_header_id             INTEGER NOT NULL,
       created_at                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_header_id) REFERENCES ${constants.HEADER_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.BPR_TABLE} (
       id                                        INTEGER PRIMARY KEY AUTOINCREMENT,
       segment_order                             INTEGER NOT NULL,
-      
+
       transaction_handling_code                 VARCHAR(2) NOT NULL,
       monetary_amount                           DECIMAL(18,2) NOT NULL,
       credit_debit_flag                         VARCHAR(1) NOT NULL,
@@ -68,55 +68,53 @@ export function create835Tables(): SqliteDatabaseType {
       dfi_id_number_qualifier_3                 VARCHAR(2),
       dfi_id_number_3                           VARCHAR(12),
       account_number_qualifier_3                VARCHAR(20),
-      account_number_3                          VARCHAR(35)
+      account_number_3                          VARCHAR(35),
 
-      
       x12_header_id                             INTEGER NOT NULL,
       created_at                                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_header_id) REFERENCES ${constants.HEADER_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.NTE_TABLE} (
       id                        INTEGER PRIMARY KEY AUTOINCREMENT,
       segment_order             INTEGER NOT NULL,
-      
+
       note_reference_code       VARCHAR(3),
       description               VARCHAR(80),
-      
+
       x12_header_id             INTEGER NOT NULL,
       created_at                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_header_id) REFERENCES ${constants.HEADER_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.TRN_TABLE} (
       id                        INTEGER PRIMARY KEY AUTOINCREMENT,
       segment_order             INTEGER NOT NULL,
-      
+
       trace_type_code           VARCHAR(2) NOT NULL,
       reference_id_1            VARCHAR(80) NOT NULL,
       originating_company_id    VARCHAR(10),
       reference_id_2            VARCHAR(80),
 
-      
       x12_header_id             INTEGER NOT NULL,
       created_at                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_header_id) REFERENCES ${constants.HEADER_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.CUR_TABLE} (
       id                                        INTEGER PRIMARY KEY AUTOINCREMENT,
       segment_order                             INTEGER NOT NULL,
-      
+
       entity_id_code_1                          VARCHAR(3) NOT NULL,
       currency_code_1                           VARCHAR(3) NOT NULL,
-      exchange_rate_1                           DECIMAL(10, 6)
-      
+      exchange_rate_1                           DECIMAL(10, 6),
+
       entity_id_code_2                          VARCHAR(3),
       currency_code_2                           VARCHAR(3),
 
@@ -130,7 +128,6 @@ export function create835Tables(): SqliteDatabaseType {
       date_2                                    DATE,
       time_2                                    TIME(2),
 
-
       date_time_qualifier_3                     VARCHAR(3),
       date_3                                    DATE,
       time_3                                    TIME(2),
@@ -143,11 +140,11 @@ export function create835Tables(): SqliteDatabaseType {
       date_5                                    DATE,
       time_5                                    TIME(2),
 
-      x12_header_id                 INTEGER NOT NULL,
-      created_at                    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      x12_header_id                             INTEGER NOT NULL,
+      created_at                                TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_header_id) REFERENCES ${constants.HEADER_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.N1_TABLE} (
@@ -161,12 +158,11 @@ export function create835Tables(): SqliteDatabaseType {
       entity_relationship_code          VARCHAR(2),
       related_entity_identifier_code    VARCHAR(3),
 
-
       created_at                        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       parent_type                       VARCHAR(50) NOT NULL,   -- e.g., 'x12_n1', 'clp_x12'
-      parent_id                         INTEGER NOT NULL, 
+      parent_id                         INTEGER NOT NULL
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.N2_TABLE} (
@@ -180,7 +176,7 @@ export function create835Tables(): SqliteDatabaseType {
       created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_n1_id) REFERENCES ${constants.N1_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.N3_TABLE} (
@@ -194,7 +190,7 @@ export function create835Tables(): SqliteDatabaseType {
       created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_n1_id) REFERENCES ${constants.N1_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.N4_TABLE} (
@@ -207,13 +203,13 @@ export function create835Tables(): SqliteDatabaseType {
       location_qualifier          VARCHAR(2),
       location_id                 VARCHAR(30),
       country_subdivision_code    VARCHAR(3),
-      postal_code_formatted       VARCHAR(20)
+      postal_code_formatted       VARCHAR(20),
 
       x12_n1_id                   INTEGER NOT NULL,
       created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_n1_id) REFERENCES ${constants.N1_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.REF_TABLE} (
@@ -221,12 +217,12 @@ export function create835Tables(): SqliteDatabaseType {
       segment_order         INTEGER NOT NULL,
 
       id_qualifier_1        VARCHAR(3) NOT NULL,
-      id_1                  VARCHAR(80) NOT NULL, 
+      id_1                  VARCHAR(80) NOT NULL,
       description           VARCHAR(80),
 
       id_qualifier_2        VARCHAR(3) NOT NULL,    -- REF04-01
       id_2                  VARCHAR(80) NOT NULL,   -- REF04-02
-      
+
       id_qualifier_3        VARCHAR(3),             -- REF04-03
       id_3                  VARCHAR(80),            -- REF04-04
 
@@ -237,7 +233,7 @@ export function create835Tables(): SqliteDatabaseType {
       parent_id             INTEGER NOT NULL,
       created_at            TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.PER_TABLE} (
@@ -246,7 +242,7 @@ export function create835Tables(): SqliteDatabaseType {
 
       contact_function_code               VARCHAR(2) NOT NULL,
       name                                VARCHAR(60),
-      
+
       communication_number_qualifier_1    VARCHAR(2),
       communication_number_1              VARCHAR(2048),
 
@@ -262,7 +258,7 @@ export function create835Tables(): SqliteDatabaseType {
       parent_id                           INTEGER NOT NULL,
       created_at                          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.RDM_TABLE} (
@@ -278,20 +274,20 @@ export function create835Tables(): SqliteDatabaseType {
       ref_id_qualifier_1_2                VARCHAR(3),             -- RDM04-03
       ref_id_1_2                          VARCHAR(80),            -- RDM04-04
       ref_id_qualifier_1_3                VARCHAR(3),             -- RDM04-05
-      ref_id_1-3                          VARCHAR(80),            -- RDM04-06
+      ref_id_1_3                          VARCHAR(80),            -- RDM04-06
 
       ref_id_qualifier_2_1                VARCHAR(3) NOT NULL,    -- RDM05-01
       ref_id_2_1                          VARCHAR(80) NOT NULL,   -- RDM05-02
       ref_id_qualifier_2_2                VARCHAR(3),             -- RDM05-03
       ref_id_2_2                          VARCHAR(80),            -- RDM05-04
       ref_id_qualifier_2_3                VARCHAR(3),             -- RDM05-05
-      ref_id_2-3                          VARCHAR(80),            -- RDM05-06
+      ref_id_2_3                          VARCHAR(80),            -- RDM05-06
 
       x12_1000_id                         INTEGER,
       created_at                          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY (x12_1000_id) REFERENCES ${constants.X12_1000_TABLE}(id) ON DELETE CASCADE 
+      FOREIGN KEY (x12_1000_id) REFERENCES ${constants.X12_1000_TABLE}(id) ON DELETE CASCADE
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.DTM_TABLE} (
@@ -309,7 +305,7 @@ export function create835Tables(): SqliteDatabaseType {
       parent_id                           INTEGER NOT NULL,
       created_at                          TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`
-  );
+  ).run();
 
   db.prepare(
     `CREATE TABLE IF NOT EXISTS ${constants.X12_1000_TABLE} (
@@ -319,7 +315,7 @@ export function create835Tables(): SqliteDatabaseType {
       created_at                          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       FOREIGN KEY (x12_header_id) REFERENCES ${constants.HEADER_TABLE}(id)
     );`
-  );
+  ).run();
 
   return db;
 }
--- src/edi835Parsing/parser.ts
+++ src/edi835Parsing/parser.ts
@@ -1,5 +1,6 @@
-import { X12parser, Schema, X12grouper } from "x12-parser";
+// import { X12parser, Schema, X12grouper } from "x12-parser";
 import { Readable } from "node:stream";
+import { create835Tables } from "./library";
 
 export interface SegmentInfo {
   name: string;
@@ -7,28 +8,29 @@ export interface SegmentInfo {
 }
 
 export async function parseX12(readStream: Readable): Promise<void> {
-  const parser = new X12parser();
-  const headingSchema = new Schema("005010X221A1", {
-    start: "ST",
-    name: "Heading",
-    terminators: ["N1"],
-  });
-  const grouper = new X12grouper(headingSchema);
+  create835Tables();
+  // const parser = new X12parser();
+  // const headingSchema = new Schema("005010X221A1", {
+  //   start: "ST",
+  //   name: "Heading",
+  //   terminators: ["N1"],
+  // });
+  // const grouper = new X12grouper(headingSchema);
 
-  try {
-    const ediStream = readStream.pipe(parser).pipe(grouper);
+  // try {
+  //   const ediStream = readStream.pipe(parser).pipe(grouper);
 
-    for await (const data of ediStream) {
-      console.log(data);
-      // if (data.isGroup && data.name == "Heading") {
-      //   decodeHeading(data);
-      // }
-    }
+  //   for await (const data of ediStream) {
+  //     console.log(data);
+  //     // if (data.isGroup && data.name == "Heading") {
+  //     //   decodeHeading(data);
+  //     // }
+  //   }
 
-    console.log("Successfully finished processing the file.");
-  } catch (err) {
-    console.error("An error occurred during processing:", err);
-  }
+  //   console.log("Successfully finished processing the file.");
+  // } catch (err) {
+  //   console.error("An error occurred during processing:", err);
+  // }
 }
 
 export function decodeHeading(info: { name: string; data: SegmentInfo[] }) {}
